54d3b8ee089e4a63c0c79152249a644d52106fff
farrahtor <farrahtor@gmail.com>
2022-10-15 13:38:09 +0200

    ajout catch

--- front/js/script.js
+++ front/js/script.js
@@ -8,14 +8,19 @@ function getAllProducts() {
       console.log(products);
 
       const sectionItems = document.getElementById("items");
+      //repartition des produits dans le dom section items
       for (let product of products) {
-        sectionItems.innerHTML += `<a href="./product.html?id=${product._id}">
-        <article>
-          <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-          <h3 class="productName">${product.name}</h3>
-          <p class="productDescription">${product.description}</p>
-        </article>
-      </a>`;
+        const productCard = `<a href="./product.html?id=${product._id}">
+          <article>
+            <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+            <h3 class="productName">${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
+          </article>
+        </a>`;
+        sectionItems.innerHTML += productCard;
       }
+    });
+    .catch((err) => {
+      console.log(err);
     });
 }
